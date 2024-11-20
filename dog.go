2554a5667f963e6a0669a78c10178309a3dabf6f
Abigail <abigailekene6@gmail.com>
2024-11-20 03:04:26 -0800

    second commit

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func WhenAult(s string) string {
+	return "whe the puppy grows up it says: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Abigailtony/dog
+
+go 1.23.0
